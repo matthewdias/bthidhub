51eccb69ff83708d2932af8ae4357f03fb73c5cc
matthewdias <matthewdias@me.com>
2023-01-12 22:35:44 -0600

    Rename user folder

--- install/setup.sh
+++ install/setup.sh
@@ -1,11 +1,11 @@
 echo 0 | sudo tee /sys/class/leds/led0/brightness > /dev/null
 
-cd /home/pi
+cd /home/matthew
 
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install git -y
 
-git clone https://github.com/ruundii/bthidhub
-cd /home/pi/bthidhub/install/on_rpi
+git clone https://github.com/matthewdias/bthidhub
+cd /home/matthew/bthidhub/install/on_rpi
 sh ./on_pi_setup.sh
